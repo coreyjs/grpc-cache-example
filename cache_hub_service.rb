b0cb3f9b63b196970bb7dd8f4c5ae71855e44f3b
Corey Schaf <cschaf@gmail.com>
2020-08-02 20:45:29 -0400

    clean up

--- cache_hub_service.rb
+++ cache_hub_service.rb
@@ -5,13 +5,10 @@ require_relative 'lib/cache_services_pb'
 class CacheHubService < Cache::CacheHub::Service
   def get_status(req, _unused_call)
     puts 'Received get status request'
-    puts req.name
     Cache::StatusResponse.new(message: 'Systems are operational, nothing to see here')
   end
 
   def upload(call)
-    puts 'got something'
-
     started, elapsed_time = 0,0
     file_chunks = []
     call.each_remote_read do |r|
@@ -19,8 +16,7 @@ class CacheHubService < Cache::CacheHub::Service
     end
 
     data = file_chunks.join('')
-    File.open("out.pdf", "w") { |f| f.write(data)}
-    byebug
+    File.open("fixme.pdf", "w") { |f| f.write(data)}
 
     Cache::UploadStatus.new(Message: 'Test', Code: :Ok)
   end
